cd2c7aafb58d6b0a6aead15608a7adf0695e5a07
Muhammad Sapuan <muhammadsapuan16@gmail.com>
2023-08-16 21:03:53 +0700

    #2 demo komentar app in C# 🎉

---
+++ DemoKomentar/DemoKomentar/DemoKomentar.cs
@@ -0,0 +1,23 @@
+﻿/*************************************************************
+Nama file	: DemoKomentar.cs
+Deskripsi	: Mendemontrasikan penggunaan komentar pada C#
+Dibuat Oleh	: Muhammad Sapuan
+Tanggal		: 16/08/2023
+**************************************************************/
+
+// Menggunakan namespace System
+using System;
+
+// Membuat Kelas
+class Hello
+{
+	// Membuat metode utama
+	static void Main(string[] args)
+	{
+		// Menampilkan bilangan 0 sampai 9
+		for (int i=0; i<10; i++)
+		{
+			Console.WriteLine(i);
+		}
+	}
+}
