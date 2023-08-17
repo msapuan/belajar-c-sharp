d5426526bb29143242ba003631bb69cf465a8a67
Muhammad Sapuan <muhammadsapuan16@gmail.com>
2023-08-17 17:53:22 +0700

    hitung luas persegi panjang in C#

---
+++ PersegiPanjang/PersegiPanjang/DemoVariabel1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class DemoVariabel1
+{
+	static void Main()
+	{
+		// mendeklrasikan variabel
+		int panjang;
+		int lebar;
+		int luas;
+
+		// mengisi nilai ke dalam variabel
+		panjang = 8;
+		lebar = 6;
+
+		// melakukan perhitungan dan menampung hasilnya
+		// ke dalam variabel luas
+		luas = panjang * lebar;
+
+		// menampilkan informasi program
+		Console.WriteLine("PERSEGI PANJANG");
+
+		// menampilkan nilai variabel
+		Console.WriteLine("panjang = " + panjang);
+		Console.WriteLine("lebar = " + lebar);
+
+		// menampilkan hasil perhitungan
+		Console.WriteLine("Luas persegi panjang = " + luas);
+
+		Console.ReadLine();
+	}
+}
