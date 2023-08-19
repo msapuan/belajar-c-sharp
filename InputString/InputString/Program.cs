8e58501d3595d43431e596a181a966ccef7b6a2c
Muhammad Sapuan <muhammadsapuan16@gmail.com>
2023-08-19 17:17:31 +0700

    input string in C#

---
+++ InputString/InputString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class InputString
+{
+	static void Main()
+	{
+		string nama;
+
+		Console.Write("Masukkan nama Anda : ");
+		nama = Console.ReadLine();
+
+		Console.WriteLine("Halo " + nama + ", Selamat datang di dunia pemrograman C#");
+		Console.ReadLine();
+	}
+}
