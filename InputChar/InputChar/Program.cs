9a33af4993cac53f8333ef2bb1f9d141bf12403c
Muhammad Sapuan <muhammadsapuan16@gmail.com>
2023-08-19 17:30:41 +0700

    input char in C#

---
+++ InputChar/InputChar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class InputChar
+{
+	static void Main()
+	{
+		// deklarasikan variabel dengan tipe datanya
+		char c;
+		string s;
+
+		Console.Write("Masukkan karakter : ");
+		s = Console.ReadLine();
+
+		// konversi tipe string ke karakter
+		if (char.TryParse(s, out c))
+		{
+			Console.WriteLine("Karakter yang dimasukkan adalah '" + c + "'");
+		} 
+		else
+		{
+			//Console.WriteLine("Data yang Anda masukkan " +
+			//	"tidak dapat dikonversi " +
+			//	"ke tipe karakter");
+			Console.WriteLine("Karakter yang dimasukkan adalah '" + s + "'");
+		}
+
+		Console.ReadLine();
+	}
+} 
